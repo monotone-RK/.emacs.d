6281d04598a4b866d31f77d01f51b0d2611017c7
monotone-RK <kobaya4121@gmail.com>
2017-07-21 04:08:51 +0900

    modify gtags keymap

--- inits/60-gtags.el
+++ inits/60-gtags.el
@@ -8,6 +8,11 @@
          (local-set-key "\C-t" 'gtags-pop-stack)
          ))
 
+(setq gtags-select-mode-hook
+      '(lambda ()
+	 (local-set-key "\C-m" 'gtags-select-tag)
+	 ))
+
 (add-hook 'c-mode-common-hook
           '(lambda()
              (gtags-mode 1)
