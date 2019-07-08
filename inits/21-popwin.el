3aa5ffb12db62479ed0f2885d7ecf2806f62830d
monotone-RK <kobaya4121@gmail.com>
2019-07-08 21:32:36 +0900

    popwin setting has been fixed

--- inits/21-popwin.el
+++ inits/21-popwin.el
@@ -1,9 +1,20 @@
 (require 'popwin)
 
-(setq display-buffer-function 'popwin:display-buffer)
-(setq popwin:popup-window-position 'bottom)
+(cond ((string-match "24." emacs-version) ;;; For Emacs 24
+       (setq display-buffer-function 'popwin:display-buffer)
+       (setq popwin:popup-window-position 'bottom)
 
-;; cf: https://github.com/emacs-jp/issues/issues/2
-(setq popwin:close-popup-window-timer-interval 0.5)
+       ;; cf: https://github.com/emacs-jp/issues/issues/2
+       (setq popwin:close-popup-window-timer-interval 0.5)
 
-(push '("*Occur*") popwin:special-display-config)
+       (push '("*Occur*") popwin:special-display-config))
+      (t                                  ;;; Others
+       (popwin-mode 1)
+       (setq pop-up-windows nil)
+       (setq anything-samewindow nil)
+       (push '("anything" :regexp t :height 0.5) popwin:special-display-config)
+       (push '("helm" :regexp t :height 0.5) popwin:special-display-config)
+       (push '("*Completions*" :height 0.4) popwin:special-display-config)
+       (push '("*compilation*" :height 0.4 :noselect t :stick t) popwin:special-display-config)
+       (push '("*Help*" :height 0.5 :noselect t :stick t) popwin:special-display-config))
+)
